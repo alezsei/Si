44ff4b6686dbf915bc3bf15d6e749d6a7f8f7c3e
Aleksei_Nikolaev <alezsei@yandex.ru>
2022-10-01 16:25:27 +0400

    creatnewstrong

--- program4/Program.cs
+++ program4/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Привет, как тебя зовут");
+string name = Console.ReadLine();
+if(name.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(name);
+}
